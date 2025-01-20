596ecc027c0acc49cb50f90104b0f36cb31dc30a
rlaEndals <rnwlsdn9621043@gmail.com>
2025-01-20 16:19:08 +0900

    day5

---
+++ src/day5/Day5Practice.java
@@ -0,0 +1,22 @@
+package day5;
+
+public class Day5Practice {
+    public static void main(String[] args) {
+//        // 1번 문제
+//        for(int i = 1; i <= 10; i++){
+//            System.out.println("i = " + i);
+//        }
+
+//        // 2번 문제
+//        for(int i = 1; i <= 10; i+= 2){
+//            System.out.println("i = " + i);
+//        }
+
+        //3번 문제
+        for(int i = 1; i <= 10; i++) {
+            if (i % 2 == 0){
+                System.out.println("i = " + i);
+            }
+        }
+    }
+}
---
+++ src/day5/Day5RePeat.java
@@ -0,0 +1,42 @@
+package day5;
+
+public class Day5RePeat {
+    public static void main(String[] args) {
+
+
+//        for (int i = 1; i <= 5; i++){
+//            System.out.println("i = " + i);
+//        }
+//
+//
+//        for(int i = 5; i > 0; i--){
+//            System.out.println("i = " + i);
+//    }
+
+//            // 미리 변수 생성
+//            int n;
+//            for (n = 1; n <= 5; n++) {}
+//
+//            // 바로 변수 초기화 후 진행
+//            for (int i = 1; i <= 5; i++) {}
+
+//        int count = 1;
+//        while (count <= 5){
+//            System.out.println("count = " + count);
+//            count++;
+//        }
+//
+//        int countt = 5;
+//        while (countt >= 1){
+//            System.out.println("countt = " + countt);
+//            countt--;
+//        }
+
+        int number = 1;
+        do{
+            System.out.println("number = " + number);
+            number++;
+        } while (number <= 5);
+
+    }
+}
